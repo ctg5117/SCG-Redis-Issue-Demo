c67c2c41d20f592b9b0c440432b4ec5f5414a1a4
Geissler, Christopher (Contractor) <Christopher_Geissler@comcast.com>
2019-05-03 10:35:44 -0400

    Fixing Issues with YAML, Log Debug

--- src/main/java/com/example/SCGDemoRedisMVS/ScgDemoRedisMvsApplication.java
+++ src/main/java/com/example/SCGDemoRedisMVS/ScgDemoRedisMvsApplication.java
@@ -1,12 +1,16 @@
 package com.example.SCGDemoRedisMVS;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @SpringBootApplication
+@RestController
+@EnableAutoConfiguration
 public class ScgDemoRedisMvsApplication {
 
 	public static void main(String[] args) {
@@ -15,7 +19,7 @@ public class ScgDemoRedisMvsApplication {
 
 	@Bean("userKeyResolver")
 	KeyResolver userKeyResolver() {
-		return exchange -> Mono.just(exchange.getSession().subscribe().toString());
+		return exchange -> Mono.just("test");
 	}
 
 }
